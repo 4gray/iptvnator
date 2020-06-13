d28a79ed02eb29d44fb3554c6327f5766ffbc70d
4gray <serega05@gmail.com>
2020-06-13 11:38:59 +0200

    build: update angular material, flex deps

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import {
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatListModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatInputModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     exports: [
