d319f261024c156385e5af553fbe12a5b0cfec37
4gray <serega05@gmail.com>
2020-07-03 08:55:51 +0200

    refactor: switch to iptv-playlist-parser

--- src/app/services/m3u-service.service.ts
+++ src/app/services/m3u-service.service.ts
@@ -1,30 +1,16 @@
 import { Injectable } from '@angular/core';
-import * as M3U8FileParser from 'm3u8-file-parser';
+import { parse } from 'iptv-playlist-parser';
 
 @Injectable({
     providedIn: 'root',
 })
 export class M3uService {
     /**
-     * m3u8 parser
-     */
-    m3u8FileParser = new M3U8FileParser();
-
-    /**
-     * Converts string based array to playlist object
+     * Parses string based array to playlist object
      * @param m3uArray m3u playlist as array with strings
      */
-    convertArrayToPlaylist(m3uArray: any[]): any {
-        this.m3u8FileParser.read(m3uArray.join('\n'));
-        return this.m3u8FileParser.getResult();
-    }
-
-    /**
-     * Converts string to playlist structure
-     * @param m3uString playlist as string
-     */
-    convertStringToPlaylist(m3uString: string): any {
-        this.m3u8FileParser.read(m3uString);
-        return this.m3u8FileParser.getResult();
+    parsePlaylist(m3uArray: any[]): any {
+        const playlistAsString = m3uArray.join('\n');
+        return parse(playlistAsString);
     }
 }
