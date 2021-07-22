a497f0570175618d7053b53fd47aa907e6361f17
4gray <serega05@gmail.com>
2021-07-22 12:55:11 +0200

    feat: add chinese translation
    Thanks to @JoJenH

--- src/app/settings/language.enum.ts
+++ src/app/settings/language.enum.ts
@@ -2,9 +2,10 @@
  * Enum with all languages that are available in the application
  */
 export enum Language {
-    ENGLISH = 'en',    
-    KOREAN = 'ko',     
+    ENGLISH = 'en',
+    KOREAN = 'ko',
     RUSSIAN = 'ru',
     GERMAN = 'de',
     SPANISH = 'es',
+    CHINESE = 'zh',
 }
