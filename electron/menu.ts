753b06a7a82211394ac3d03a9303760230239e71
4gray <4gray@users.noreply.github.com>
2023-03-23 23:39:24 +0100

    fix: shortcut to close the app

--- electron/menu.ts
+++ electron/menu.ts
@@ -72,6 +72,7 @@ export class AppMenu {
                 {
                     label: 'Exit',
                     click: () => app.quit(),
+                    accelerator: 'CmdOrCtrl+Q',
                 },
             ],
         });
