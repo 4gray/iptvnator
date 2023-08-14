adea3cf0d144e49cfcd832ec52b063d42b1f4e77
4gray <4gray@users.noreply.github.com>
2023-08-14 08:49:20 +0200

    refactor(stalker): extend portal related store

--- src/app/xtream/portal.store.ts
+++ src/app/xtream/portal.store.ts
@@ -3,6 +3,7 @@ import { ComponentStore } from '@ngrx/component-store';
 
 export interface PortalState {
     searchPhrase: string;
+    content: any[];
 }
 
 @Injectable({ providedIn: 'root' })
@@ -10,17 +11,28 @@ export class PortalStore extends ComponentStore<PortalState> {
     constructor() {
         super({
             searchPhrase: '',
+            content: [],
         });
     }
 
     // selectors
     readonly searchPhrase = this.selectSignal((state) => state.searchPhrase);
 
+    readonly getContentById = (id: string) =>
+        this.selectSignal((state) => state.content.find((i) => i.id === id));
+
     // reducers
     readonly setSearchPhrase = this.updater(
         (state, searchPhrase: string): PortalState => ({
             ...state,
             searchPhrase,
+        })
+    );
+
+    readonly setContent = this.updater(
+        (state, content: any[]): PortalState => ({
+            ...state,
+            content,
         })
     );
 }
