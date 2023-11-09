56f2d148cfd91cabe31e8c4c503eacdd1ad02fdc
4gray <4gray@users.noreply.github.com>
2023-11-09 10:01:02 +0100

    chore: use @angular-builders/jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'jest-preset-angular',
-    resetMocks: true,
-    setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-    testMatch: ['**/+(*.)+(spec).+(ts)?(x)'],
-    coverageReporters: ['html', 'lcov'],
-    transformIgnorePatterns: ['node_modules/(?!.*.mjs$|@datorama/akita)'],
-};
