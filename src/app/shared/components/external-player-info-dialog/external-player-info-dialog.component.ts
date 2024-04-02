cbdfbeb3ce5b5662021a5867029791b0fae12635
4gray <serega05@gmail.com>
2024-04-02 22:01:37 +0200

    refactor(portals): simplify external player info dialog

--- src/app/shared/components/external-player-info-dialog/external-player-info-dialog.component.ts
+++ src/app/shared/components/external-player-info-dialog/external-player-info-dialog.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { PlayerViewComponent } from '../../../xtream/player-view/player-view.component';
 
 @Component({
     standalone: true,
     template: `
         <h2 mat-dialog-title>{{ 'INFORMATION' | translate }}</h2>
         <mat-dialog-content class="mat-typography">
-            <app-player-view />
+            <div class="centered">
+                <mat-icon class="icon">live_tv</mat-icon>
+                <div>
+                    The video is playing in external player window.<br />
+                    Please make sure that mpv/vlc player is correctly installed
+                    on your system.<br />
+                    See
+                    <a
+                        [routerLink]
+                        style="cursor: pointer"
+                        (click)="
+                            openUrl(
+                                'https://github.com/4gray/iptvnator/wiki/What-is-mpv-video-player-and-how-to-install-it-on-different-operating-systems%3F'
+                            )
+                        "
+                        >installation instructions</a
+                    >
+                    for more details.
+                </div>
+            </div>
         </mat-dialog-content>
         <mat-dialog-actions align="end">
             <button mat-button mat-dialog-close cdkFocusInitial color="accent">
@@ -17,11 +37,31 @@ import { PlayerViewComponent } from '../../../xtream/player-view/player-view.com
             </button>
         </mat-dialog-actions>
     `,
+    styles: [
+        `
+            .centered {
+                text-align: center;
+                line-height: 24px;
+                margin: 20px;
+
+                .icon {
+                    font-size: 64px;
+                    height: 64px;
+                    width: 64px;
+                }
+            }
+        `,
+    ],
     imports: [
         MatButtonModule,
         MatDialogModule,
-        PlayerViewComponent,
+        MatIconModule,
+        RouterLink,
         TranslateModule,
     ],
 })
-export class ExternalPlayerInfoDialogComponent {}
+export class ExternalPlayerInfoDialogComponent {
+    openUrl(url: string) {
+        window.open(url, '_blank');
+    }
+}
