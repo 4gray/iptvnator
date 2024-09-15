d174056e7fe8b66df78ecc0a963934f28ebbed8c
4gray <serega05@gmail.com>
2024-09-15 21:29:14 +0200

    feat(xtream): search for channels

--- src/app/portals/live-stream-layout/live-stream-layout.component.ts
+++ src/app/portals/live-stream-layout/live-stream-layout.component.ts
@@ -6,11 +6,17 @@ import {
     EventEmitter,
     Input,
     Output,
+    signal,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { TranslateModule } from '@ngx-translate/core';
 import { XtreamItem } from '../../../../shared/xtream-item.interface';
 import { VideoPlayer } from '../../settings/settings.interface';
+import { FilterPipe } from '../../shared/pipes/filter.pipe';
 import { EpgItem } from '../../xtream/epg-item.interface';
 import { EpgViewComponent } from '../epg-view/epg-view.component';
 import { WebPlayerViewComponent } from '../web-player-view/web-player-view.component';
@@ -22,11 +28,16 @@ import { WebPlayerViewComponent } from '../web-player-view/web-player-view.compo
     styleUrls: ['./live-stream-layout.component.scss'],
     imports: [
         EpgViewComponent,
+        FilterPipe,
+        FormsModule,
         MatListModule,
+        MatIconModule,
+        MatInputModule,
+        MatFormFieldModule,
         NgIf,
         ScrollingModule,
         WebPlayerViewComponent,
-        FormsModule,
+        TranslateModule,
     ],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -37,6 +48,8 @@ export class LiveStreamLayoutComponent {
     @Input() streamUrl: string;
 
     @Output() itemClicked = new EventEmitter<XtreamItem>();
+
+    searchString = signal<string>('');
 
     trackBy(_index: number, item: XtreamItem) {
         return item.stream_id;
