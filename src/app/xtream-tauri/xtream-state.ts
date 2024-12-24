94e07a07a781fa7224e2b3fc43abbdd6f1877187
4gray <serega05@gmail.com>
2024-12-24 23:16:20 +0100

    refactor(xtream): define XtreamState interface in extra file

---
+++ src/app/xtream-tauri/xtream-state.ts
@@ -0,0 +1,30 @@
+import { XtreamCategory } from '../../../shared/xtream-category.interface';
+import { XtreamLiveStream } from '../../../shared/xtream-live-stream.interface';
+import { XtreamSerieItem } from '../../../shared/xtream-serie-item.interface';
+import { XtreamVodStream } from '../../../shared/xtream-vod-stream.interface';
+import { EpgItem } from '../xtream/epg-item.interface';
+
+export type ContentType = 'live' | 'vod' | 'series';
+
+export interface XtreamState {
+    isLoadingCategories: boolean;
+    isLoadingContent: boolean;
+    isImporting: boolean;
+    liveCategories: XtreamCategory[];
+    vodCategories: XtreamCategory[];
+    serialCategories: XtreamCategory[];
+    liveStreams: XtreamLiveStream[];
+    vodStreams: XtreamVodStream[];
+    serialStreams: XtreamSerieItem[];
+    page: number;
+    limit: number;
+    selectedCategoryId: number | null;
+    searchResults: any[];
+    selectedContentType: ContentType;
+    selectedItem: any | null;
+    importCount: number;
+    itemsToImport: number;
+    currentPlaylist: any | null;
+    epgItems: EpgItem[];
+    hideExternalInfoDialog: boolean;
+}
