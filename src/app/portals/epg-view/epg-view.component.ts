23b967aad748162edd378e04e125aa6d5bbfe0f9
4gray <serega05@gmail.com>
2024-12-26 22:20:30 +0100

    feat(xtream): enhance EPG view with current program highlighting and progress indicator

--- src/app/portals/epg-view/epg-view.component.ts
+++ src/app/portals/epg-view/epg-view.component.ts
@@ -2,7 +2,6 @@ import { DatePipe } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
-import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { TranslateModule } from '@ngx-translate/core';
@@ -13,29 +12,32 @@ import { EpgItem } from '../../xtream/epg-item.interface';
     standalone: true,
     selector: 'app-epg-view',
     templateUrl: './epg-view.component.html',
-    imports: [
-        DatePipe,
-        MatIconButton,
-        MatDivider,
-        MatIcon,
-        MatListModule,
-        TranslateModule,
-    ],
-    styles: [
-        `
-            .epg-title {
-                margin: 0;
-                font-size: 1.2rem;
-                font-weight: 500;
-                padding: 16px;
-            }
-        `,
-    ],
+    imports: [DatePipe, MatIconButton, MatIcon, MatListModule, TranslateModule],
+    styleUrls: ['./epg-view.component.scss'],
 })
 export class EpgViewComponent {
     @Input() epgItems: EpgItem[];
 
     dialog = inject(MatDialog);
+
+    isCurrentProgram(item: EpgItem): boolean {
+        const end = item.stop ?? item.end;
+        const now = new Date().getTime();
+        const start = new Date(item.start).getTime();
+        const stop = new Date(end).getTime();
+        return now >= start && now <= stop;
+    }
+
+    getProgress(item: EpgItem): number {
+        const now = new Date().getTime();
+        const start = new Date(item.start).getTime();
+        const end = new Date(item.stop ?? item.end).getTime();
+
+        const total = end - start;
+        const current = now - start;
+
+        return Math.min(Math.max((current / total) * 100, 0), 100);
+    }
 
     showDetails(item: EpgItem) {
         this.dialog.open(EpgItemDescriptionComponent, {
