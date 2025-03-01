4cd7ab0afe4671314982254e88ac137256470d67
4gray <serega05@gmail.com>
2025-03-01 18:43:56 +0100

    refactor(electron): remove unused preload, HTML, and server files

--- electron/preload.ts
+++
@@ -1,7 +0,0 @@
-import { Titlebar, TitlebarColor } from 'custom-electron-titlebar';
-
-window.addEventListener('DOMContentLoaded', () => {
-    new Titlebar({
-        backgroundColor: TitlebarColor.fromHex('#1b1c1c'),
-    });
-});
